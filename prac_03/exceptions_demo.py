bb299a6d82f1cdd486101a62b3d701376b786819
Egide Ndazimana <jd160317@ad.jcu.edu.au>
2025-09-30 14:32:10 +1000

    Refactor exceptions demo program to avoid a ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,12 +9,14 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    # Make sure that the denominator is non-zero.
+    while denominator == 0:
+        print("Denominator cannot be zero.")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
 
 # Answer to question 1
