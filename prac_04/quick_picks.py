84bb52e29647612cacecf3584dc626305bf027b6
Egide Ndazimana <jd160317@ad.jcu.edu.au>
2025-10-09 21:03:47 +1000

    Add quick picks program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical
+"""
+import random
+
+from ipywidgets.widgets.widget_tagsinput import NumbersInputBase
+
+NUMBERS_PER_LINE = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        numbers = produce_unique_integers(NUMBERS_PER_LINE, MIN_NUMBER, MAX_NUMBER)
+        numbers.sort()  # sort the numbers in ascending order
+        # convert the numbers to strings and pad them with whitespace on the left to fill a length of 2
+        padded_numbers = [f"{number:2}" for number in numbers]
+        print(" ".join(padded_numbers))  # join the padded numbers with a space in between and print the result
+
+
+def produce_unique_integers(number_of_integers: int, lower_bound: int, upper_bound: int):
+    """Produce a list of unique, random integers between given bounds (inclusive)."""
+    integers = []
+    while len(integers) != number_of_integers:
+        integer = random.randint(lower_bound, upper_bound)
+        # Make sure appending integer to integers will not result in repeated values
+        if integer not in integers:
+            integers.append(integer)
+    return integers
+
+
+main()
