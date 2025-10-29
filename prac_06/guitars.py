ce4788e5a77c45b85a3f05d6b728de00cf5ac614
Egide Ndazimana <egide.n1997@gmail.com>
2025-10-29 12:33:47 +1000

    Complete prac 6 guitar exercise

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,26 +4,33 @@ from guitar import Guitar
 def main():
     print("My guitars!")
     guitars = []
-    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    # while True:
-    #     name = input("Name: ")
-    #     if name == "":
-    #         break
-    #     year = int(input("Year: "))
-    #     cost = float(input("Cost: "))
-    #     guitar = Guitar(name, year, cost)
-    #     print(f"{guitar} added.")
-    #     print()
-    #     guitars.append(guitar)
+
+    # Populate guitars list with Guitar class instances
+    while True:
+        name = input("Name: ")
+        if name == "":
+            break
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        print()
+
+    # Display all the guitars
+    print()
     print("These are my guitars:")
+    max_name_length = max([len(guitar.name) for guitar in guitars])
+    # Create a list of cost values where each is formatted with a comma separator and two decimal places
+    # e.g. 3456.7 -> "3,456.70"
+    formatted_costs = [f"{guitar.cost:1,.2f}" for guitar in guitars]
+    max_formatted_cost_length = max([len(formatted_cost) for formatted_cost in formatted_costs])
     for i, guitar in enumerate(guitars):
-        max_name_length = max([len(guitar.name) for guitar in guitars])
-        max_cost_length = max([len(str(guitar.cost)) for guitar in guitars])
-        formatted_name = f"{guitar.name:>{max_name_length}}"
-        formatted_cost = f"{guitar.cost:>10,.{max_cost_length}}"
-        print(f"Guitar {i + 1}: {formatted_name} ({guitar.year}), worth $ {formatted_cost}")
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        right_aligned_name = f"{guitar.name:>{max_name_length}}"
+        right_aligned_formatted_cost = f"{formatted_costs[i]:>{max_formatted_cost_length}}"
+        print(
+            f"Guitar {i + 1}:  {right_aligned_name} ({guitar.year}), worth $ {right_aligned_formatted_cost}{vintage_string}")
 
 
 main()
