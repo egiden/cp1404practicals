65d7043a3d890579cf8dabe0f5715c5ed54c4172
Egide Ndazimana <egide.n1997@gmail.com>
2025-11-02 13:02:25 +1000

    Refactor prac 6 display_guitars function to make it more readable and to add more comments

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -22,12 +22,16 @@ def main():
 
 
 def display_guitars(guitars: list[Guitar]):
-    print("These are my guitars:")
-    max_name_length = max([len(guitar.name) for guitar in guitars])
+    """Print a formatted list of the details of guitars from a list of Guitar instances."""
+    max_name_length = max([len(guitar.name) for guitar in guitars])  # Calculate length of longest guitar name
     # Create a list of cost values where each is formatted with a comma separator and two decimal places
     # e.g. 3456.7 -> "3,456.70"
     formatted_costs = [f"{guitar.cost:1,.2f}" for guitar in guitars]
+    # Calculate length of the longest element of formatted_costs
     max_formatted_cost_length = max([len(formatted_cost) for formatted_cost in formatted_costs])
+
+    # Print each guitar's details
+    print("These are my guitars:")
     for i, guitar in enumerate(guitars):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         right_aligned_name = f"{guitar.name:>{max_name_length}}"
